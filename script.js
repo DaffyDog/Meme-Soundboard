83114a242ad8bcc68636bb9abf99a2e4fbcfbfef
DaffyDog <151995589+DaffyDog@users.noreply.github.com>
2026-05-25 17:08:24 +1000

    Initial Upload

---
+++ script.js
@@ -0,0 +1,62 @@
+const creeper = new Audio("public/Sounds/creeper.mp3")
+const happy = new Audio("public/Sounds/happy.mp3")
+const wow = new Audio("public/Sounds/wow.mp3")
+const fail = new Audio("public/Sounds/spongebob-fail.mp3")
+const jet2 = new Audio("public/Sounds/jet2.mp3")
+const later = new Audio("public/Sounds/later.mp3")
+const amongus = new Audio("public/Sounds/among-us.mp3")
+const kid = new Audio("public/Sounds/kid.mp3")
+const oof = new Audio("public/Sounds/oof.mp3")
+const chicken = new Audio("public/Sounds/chicken-banana.mp3")
+const doh = new Audio("public/Sounds/doh.mp3")
+const naplan = new Audio("public/Sounds/naplan.mp3")
+
+
+function playCreeper() {
+    creeper.play()
+    console.log("Creeper aw man...")
+}
+function playHappyCat() {
+   happy.play()
+   console.log("Happy Happy Happy")
+}
+function playWow() {
+   wow.play()
+   console.log("Wow!!")
+}
+function playFail() {
+   fail.play()
+   console.log("Fail")
+}
+function playJet2() {
+   jet2.play()
+   console.log("Nothing Beats A Jet 2 Holiday")
+}
+function playLater() {
+    later.play()
+    console.log("A few moments later")
+}
+function playAmongUs() {
+    amongus.play()
+    console.log("Imposter")
+}
+function playKid() {
+    kid.play()
+    console.log("ever since i was a kid i am steve")
+}
+function playOof() {
+    oof.play()
+    console.log("oof")
+}
+function playChickenBanana() {
+    chicken.play()
+    console.log("Chicken Banana")
+}
+function playDoh() {
+    doh.play()
+    console.log("D'oh")
+}
+function playNAPLAN() {
+    naplan.play()
+    console.log("RIP")
+}
